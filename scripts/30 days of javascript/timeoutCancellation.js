5079dd8089fe8fc874cdb532d078057b8ece63bc
Tahira Jannat <jannat.setu95@gmail.com>
2024-03-21 15:58:34 +0600

    timeout cancelation

--- scripts/30 days of javascript/timeoutCancellation.js
+++ scripts/30 days of javascript/timeoutCancellation.js
@@ -1,32 +1,17 @@
 var cancellable = function (fn, args, t) {
-    const timer = setTimeout(() => fn(...args), t);
-    return () => {
-        clearTimeout(timer);
+    const timerId = setTimeout(() => {
+        const result = fn(...args);
+        const output = { time: t, returned: result };
+        const output2 = JSON.stringify(output);
+        console.log(JSON.stringify(output));
+    }, t);
+    return function cancelFn() {
+        clearTimeout(timerId);
     };
 };
 
-const result = [];
-
 const fn = (x) => x * 5;
-const args = [2],
-    t = 20,
-    cancelT = 50;
-
-const start = performance.now();
-
-const log = (...argsArr) => {
-    const diff = Math.floor(performance.now() - start);
-    result.push({ time: diff, returned: fn(...argsArr) });
-};
-
-const cancel = cancellable(log, args, t);
-
-const maxT = Math.max(t, cancelT);
-
-setTimeout(() => {
-    cancel();
-}, cancelT);
-
-setTimeout(() => {
-    console.log(result); // [{"time":20,"returned":10}]
-}, maxT + 15);
+const args = [2];
+const t = 20;
+const cancelFn = cancellable(fn, args, t);
+// console.log(JSON.stringify(output));
