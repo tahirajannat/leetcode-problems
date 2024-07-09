456e1d7314530dba9470d24f7638ca1abb9f7043
Tahira Jannat <jannat.setu95@gmail.com>
2024-07-09 23:46:18 +0200

    quick sort

---
+++ scripts/DSA/quickSort.js
@@ -0,0 +1,13 @@
+function quickSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        let key = arr[i];
+        for (var j = i - 1; j >= 0, arr[j] > key; j--) {
+            arr[j + 1] = arr[j];
+        }
+
+        arr[j + 1] = key;
+        console.log('insertionSort', arr);
+    }
+}
+var arr = [234, 235, 43, 55, 63, 5, 6, 547];
+quickSort(arr);
